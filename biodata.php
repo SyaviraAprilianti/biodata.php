2d7ffc132b7dae1dc0aebac539ab88851b9ad669
SyaviraAprilianti <55771242+SyaviraAprilianti@users.noreply.github.com>
2020-04-09 10:16:11 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,6 @@
+<?php
+echo'Nama           : Syavira Aprilianti <br>';
+echo'Kelas          : XI RPL 1<br>';
+echo'Asal Sekolah   : SMK Taruna Bhakti Depok <br>';
+echo'Alamat         : Kp. Bendungan Rt 02 Rw 02 Kelurahan Cilodong Kecamatan Cilodong ';
+?>
